0e81d4a380c8ed9a23649280080ba7f8c40a2561
monje122 <166882577+monje122@users.noreply.github.com>
2025-12-17 11:32:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// ==================== CONFIGURACIÓN INICIAL ====================
-const supabaseUrl = 'https://dbkixcpwirjwjvjintkr.supabase.co';
-const supabase = window.supabase.createClient(supabaseUrl, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRia2l4Y3B3aXJqd2p2amludGtyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYwNjYxNDksImV4cCI6MjA2MTY0MjE0OX0.QJmWLWSe-pRYwxWeel8df7JLhNUvMKaTpL0MCDorgho');
 
 // ==================== VARIABLES GLOBALES ====================
 let cartonesOcupados = [];
